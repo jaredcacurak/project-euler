8a4c3f0edecdea06eab77f764fa5cef94477ff2b
Jared Cacurak <jared.cacurak@gmail.com>
2013-07-19 21:26:27 -0400

    Use a generator to return the multiples of three or five.

--- problem-1/problem_1.py
+++ problem-1/problem_1.py
@@ -1,1 +1,16 @@
-sum([n for n in range(1, 1000) if n % 3 == 0 or n % 5 == 0])
+def multiples_of_three_or_five(below):
+	"""Generator for all the natural numbers that are multiples of 3 or 5."""
+	next = 0
+	index = 0
+
+	# Calcuate the steps
+	# [3, 2, 1, 3, 1, 2, 3]
+	steps = [abs(3 - (n % 6)) or 3 for n in range(7)]
+
+	while below > next:
+		yield next
+		next = next + steps[index]
+		index = index + 1 if index < 6 else 0
+
+# Sum of all the multiple of 3 or 5 below 1000
+print sum(n for n in multiples_of_three_or_five(1000))
